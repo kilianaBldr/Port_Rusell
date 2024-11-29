dc253d945e4e3ac17bc9a004908e694724ad7a61
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-11-29 14:41:45 +0100

    Connexion MangoDb effectué

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const mangodb = require('./db/mango');
 
-var app = express();
+mangodb.initClientDbConnection();
+
+const app = express();
+
+app.use(
+    cors({
+        exposedHeaders: ['Authorization'],
+        origin: '*',
+    })
+);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +24,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use((req, res) => {
+    res.status(404).json({
+        name: 'API',
+        version: '1.0',
+        status: 404,
+        message: 'not_found',
+    });
+});
 
 module.exports = app;
---
+++ db/mango.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const clientOptions = {
+
+    dbName            : 'apinode'
+};
+
+exports.initClientDbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.URL_MANGO, clientOptions)
+        console.log('Connection MangoDB effectué');
+    }catch (error) {
+        console.log("Erreur de connexion MongoDB :", error);
+        throw error;
+    }
+}
